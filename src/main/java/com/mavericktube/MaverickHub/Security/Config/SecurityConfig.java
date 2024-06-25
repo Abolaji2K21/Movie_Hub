d0465596332faa26f0a3131849b2c961a2a497bd
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-25 15:09:26 +0100

    Passencoder

--- src/main/java/com/mavericktube/MaverickHub/Security/Config/SecurityConfig.java
+++ src/main/java/com/mavericktube/MaverickHub/Security/Config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.mavericktube.MaverickHub.Security.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,5 +13,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(){
             return null;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return  null;
     }
 }
