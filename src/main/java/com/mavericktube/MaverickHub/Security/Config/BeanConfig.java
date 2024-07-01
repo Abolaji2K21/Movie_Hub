093cd1fd3bd3ed8f6f1a058663668989528298d1
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-01 17:08:58 +0100

    Still Implementing

--- src/main/java/com/mavericktube/MaverickHub/Security/Config/BeanConfig.java
+++ src/main/java/com/mavericktube/MaverickHub/Security/Config/BeanConfig.java
@@ -16,7 +16,7 @@ public class BeanConfig {
         return new BCryptPasswordEncoder();
     }
 
-    public static void main(String[] args) {
-        System.out.println(new BCryptPasswordEncoder().encode("password"));
-    }
+//    public static void main(String[] args) {
+//        System.out.println(new BCryptPasswordEncoder().encode("password"));
+//    }
 }
