674468b76574e901fca6d28fc8ead31de354ecd4
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-01 14:41:58 +0100

    AUTHORIZATION

--- src/main/java/com/mavericktube/MaverickHub/Security/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/mavericktube/MaverickHub/Security/filters/CustomAuthorizationFilter.java
@@ -10,6 +10,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static com.mavericktube.MaverickHub.Security.utils.SecurityUtils.PUBLIC_ENDPOINT;
+
 @Component
 public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
@@ -18,7 +20,21 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        //1.
+        //1.  Retrieve Request Path
+        //1b. if the request path from 1a is to a public path, call the
+        //      next filter in chain and terminate this filters execution.
+
+        //2a. Retrieve access token from the request header,
+        //3.  decode access token
+        //4.  extract token permission
+        //5/ add token permission to security context
+        //    call next filter in the filter chain
+        String requestPath = request.getServletPath();
+        boolean isRequestPathPublic = PUBLIC_ENDPOINT.contains(requestPath);
+        if(isRequestPathPublic) filterChain.doFilter(request,response);
+        request.getHeader("AUTHORIZATION");
+
+
 
 
 
---
+++ src/main/java/com/mavericktube/MaverickHub/Security/utils/SecurityUtils.java
@@ -0,0 +1,10 @@
+package com.mavericktube.MaverickHub.Security.utils;
+
+import java.util.List;
+
+public class SecurityUtils {
+    private SecurityUtils() {
+    }
+
+    public static final List<String> PUBLIC_ENDPOINT = List.of("/api/v1/auth");
+}
