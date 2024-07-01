ffcc3fd575e2d2c85c3f4ea4302f40f578dfbef5
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-07-01 14:55:18 +0100

    Decode the token

--- src/main/java/com/mavericktube/MaverickHub/Security/filters/CustomAuthorizationFilter.java
+++ src/main/java/com/mavericktube/MaverickHub/Security/filters/CustomAuthorizationFilter.java
@@ -5,11 +5,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+import static com.mavericktube.MaverickHub.Security.utils.SecurityUtils.JWT_PREFIX;
 import static com.mavericktube.MaverickHub.Security.utils.SecurityUtils.PUBLIC_ENDPOINT;
 
 @Component
@@ -32,7 +34,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
         String requestPath = request.getServletPath();
         boolean isRequestPathPublic = PUBLIC_ENDPOINT.contains(requestPath);
         if(isRequestPathPublic) filterChain.doFilter(request,response);
-        String authorizationRequest = request.getHeader("AUTHORIZATION");
-
+        String authorizationRequest = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String token = authorizationRequest.substring(JWT_PREFIX.length()).strip();
     }
 }
--- src/main/java/com/mavericktube/MaverickHub/Security/utils/SecurityUtils.java
+++ src/main/java/com/mavericktube/MaverickHub/Security/utils/SecurityUtils.java
@@ -7,4 +7,6 @@ public class SecurityUtils {
     }
 
     public static final List<String> PUBLIC_ENDPOINT = List.of("/api/v1/auth");
+
+    public static final String JWT_PREFIX = "Bearer";
 }
